85f0fd619464198c2b025529e413d0d706a9d725
Simsek <cihan.simsek@sap.com>
2019-01-12 13:59:53 +0100

    Done

--- script.js
+++ script.js
@@ -36,12 +36,27 @@ WARNING!!! WARNING!!!
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
+(function tellMeNames(){
+  for (var i =0; i <names.length; i++) {
+         var firstLetter = names[i].charAt(0).toLowerCase();
+         if (firstLetter==="j") {
+            byeSpeaker.name = names[i];
+            byeSpeaker.speak();
+         }else{
+            helloSpeaker.name = names[i];
+            helloSpeaker.speak();
+         }
+
+         } ;
+})()
+
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+/*for (/* fill in parts of the 'for' loop to loop over names array */ //) {
+
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
@@ -57,9 +72,9 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
+  //if (/* fill in condition here */) {
     // byeSpeaker.xxxx
-  } else {
+  //} else {
     // helloSpeaker.xxxx
-  }
-}
+  //}
+//}*/
